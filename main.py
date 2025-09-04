7da221da2bc6f1e37eb55a8da84a8aabdd4e8ec9
ravikantpal1408 <ravikantpal123@gmail.com>
2025-09-04 09:15:32 +0530

    initial setup done

---
+++ main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from app.api import user_controller
+
+app = FastAPI()
+
+app.include_router(user_controller.router, prefix="/api", tags=["users"])
+
+# app.get("/")(lambda: {"message": "Hello World"})
