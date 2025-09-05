5b8659355e4c6f1ab916e107ca9efbe381822cc9
ravikantpal1408 <ravikantpal123@gmail.com>
2025-09-05 23:42:14 +0530

    commit for the current branch

--- main.py
+++ main.py
@@ -4,5 +4,3 @@ from app.api import user_controller
 app = FastAPI()
 
 app.include_router(user_controller.router, prefix="/api", tags=["users"])
-
-# app.get("/")(lambda: {"message": "Hello World"})
