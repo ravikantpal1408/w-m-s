e6162381aacdf3e661af948e506f910c011d767b
ravikantpal <ravikantpal@ravikantpals-MacBook-Pro.local>
2025-10-03 16:32:38 +0530

    continuing work on forms

--- client-app/src/components/treatment-plants/PlantRegistration.tsx
+++ client-app/src/components/treatment-plants/PlantRegistration.tsx
@@ -1,4 +1,13 @@
-import { Box, FormGroup, Grid, TextField } from "@mui/material";
+import {
+  Box,
+  FormControl,
+  FormGroup,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
 import * as React from "react";
 import PageContainers from "../../containers/PageContainers";
 
@@ -45,9 +54,35 @@ const PlantRegistration: React.FC = () => {
               />
             </Grid>
             <Grid size={{ xs: 6, sm: 3 }} sx={{ display: "flex" }}>
+              <FormControl fullWidth>
+                <InputLabel id="treatment-plant-select-label">Type</InputLabel>
+                <Select
+                  labelId="treatment-plant-select-label"
+                  id="treatment-plant-select"
+                  value={''}
+                  label="Age"
+                  onChange={(e) => console.log(e.target.value)}
+                  required
+                >
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+             <Grid size={{ xs: 6, sm: 3 }} sx={{ display: "flex" }}>
               <TextField
-                id="plant-Type"
-                label="Plant Type"
+                id="plant-owner"
+                label="Plant Owner"
+                variant="outlined"
+                required
+                fullWidth
+              />
+            </Grid>
+            <Grid size={{ xs: 6, sm: 3 }} sx={{ display: "flex" }}>
+              <TextField
+                id="licence-number"
+                label="Licence Number"
                 variant="outlined"
                 required
                 fullWidth
